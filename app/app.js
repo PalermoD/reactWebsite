9c949da5bbd87401ecd40b257930fa76376db545
PalermoD <palermo.deschamps@gmail.com>
2016-05-06 15:00:41 -0400

    style sheets loaded in app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
-document.write('this is from app.js' + '</br>');
+require("!style!css!../stylesheets/styles.css");
 
+document.write('this is from app.js' + '</br>');
 document.write(require('./test.js'));
